0c8c34051b461f977ce9f9610293f9213521e124
Daniel C. Daugherty <daniel@rabidferrets.com>
2022-08-02 09:26:57 -0700

    Setuptools config

--- lproject/__version__.py
+++ lproject/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "looker_project_utility"
 __description__ = "Looker Project Utility"
 __url__ = ""
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = "Daniel C. Daugherty, BytecodeIO"
 __author_email__ = "daniel.daugherty@bytecode.io"
 __license__ = None
